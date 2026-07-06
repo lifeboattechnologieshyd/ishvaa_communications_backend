766d953fc72bbbe237cc2fc812ad1078930b9320
charanlakkam331 <charanlakkam0309@gmail.com>
2026-07-06 22:32:37 +0530

    test api added

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from user.views import TestAPIView
 
 urlpatterns = [
-    path("test/", TestAPIView.as_view(), name="test-api"),
+    path("test", TestAPIView.as_view(), name="test-api"),
 
 
 ]
